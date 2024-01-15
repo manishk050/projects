285e7053a0e3136ed26f439e2f7e10d77c4a2f42
mikey005 <manishmonu1357@gmail.com>
2024-01-15 20:55:13 +0530

    first comit

---
+++ Simple Madlib.py
@@ -0,0 +1,20 @@
+adj1=input("Enter a adjective")
+name=input("Enter a name")
+noun1=input("Enter a noun")
+emo1=input("Enter a emotion")
+adj2=input("you wonder how many question?, Give me a adjective ill tell")
+animal=input("one more question, ok Enter a animal")
+occ=input("one more, Enter a occupation")
+pronoun=input("last question, Enter a pronoun")
+noun2=input("only one more, Give me a noun")
+adj3=input("OK Listen, This is last, Enter a adjective")
+adj4=input("OK FINALE, Enter a adjective")
+emo2=input("OK OK DONE DONE, put off one more emotion")
+
+print(f"In a whimsical land filled with {adj1} creatures and vibrant hues, a peculiar adventure began.\n"
+      f"Our fearless protagonist, {name}, stumbled upon a mysterious {noun1} that seemed to hold the key to unlocking the secrets of the enchanted realm.\n"
+      f"With a sense of {emo1}, they embarked on a quest to discover the truth behind the magical phenomenon. Along the way, they\n"
+      f"encountered a quirky cast of characters, including a {adj2} wizard, a mischievous {animal}, and a wise old {occ}. Each encounter added a new layer of intrigue to the unfolding tale. As\n"
+      f"the journey continued, challenges arose that tested {pronoun}'s courage and wit. From navigating treacherous {noun2} to solving perplexing riddles, every twist and turn brought them closer to unraveling the ultimate mystery. Alongside loyal companions, like the {adj3} sidekick and the {adj4} mentor, our hero faced both\n"
+      f"whimsy and peril with a blend of determination and {emo2}. Little did they know that the final revelation would not only reshape their understanding of the fantastical world but also\n"
+      f"leave an indelible mark on the hearts of all who dared to join them on this extraordinary escapade.")
